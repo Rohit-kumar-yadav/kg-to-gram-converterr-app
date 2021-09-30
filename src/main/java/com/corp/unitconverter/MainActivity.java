9c7c60d1febea9e42a0d752319e7c3598f177af9
Rohit kumar yadav <ry256423@gmail.com>
2021-09-30 11:05:17 +0530

    v1.0

---
+++ src/main/java/com/corp/unitconverter/MainActivity.java
@@ -0,0 +1,41 @@
+package com.corp.unitconverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+   private Button btn;
+   TextView textView;
+   EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+        editText = findViewById(R.id.editText);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Successfully Converted", Toast.LENGTH_SHORT).show();
+                 int kg = Integer.parseInt(editText.getText().toString());
+
+                 double gram = kg*1000;
+
+                 textView.setText("Value in gram is: "+gram);
+
+
+            }
+        });
+    }
+}
